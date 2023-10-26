3c5cda62d3d2abe3e7c8ad075e8ed92cb59c9845
cute511131 <cute511131@gmail.com>
2023-10-26 11:54:51 +0800

    111

---
+++ 2023.10.26/datasource.py
@@ -0,0 +1,54 @@
+import requests
+import sqlite3
+
+__all__ = ['updata_sqlite_data']
+
+def __download_youbike_data()->list[dict]:
+    '''
+    下載台北市youbike資料2.0
+    https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json
+    '''
+    youbike_url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
+    response = requests.get(youbike_url)
+    response.raise_for_status()
+    print("下載成功")
+    return response.json()
+
+def __create_table(conn:sqlite3.Connection):    
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        CREATE TABLE  IF NOT EXISTS 台北市youbike(
+            "id"	INTEGER,
+            "站點名稱"	TEXT NOT NULL,
+            "行政區"	TEXT NOT NULL,
+            "更新時間"	TEXT NOT NULL,
+            "地址"	TEXT,
+            "總車輛數"	INTEGER,
+            "可借"	INTEGER,
+            "可還"	INTEGER,
+            PRIMARY KEY("id" AUTOINCREMENT)
+        );
+        '''
+    )
+    conn.commit()
+
+def __insert_data(conn:sqlite3.Connection,values:list[any])->None:
+    cursor = conn.cursor()
+    sql = '''
+    INSERT INTO 台北市youbike(站點名稱,行政區,更新時間,地址,總車輛數,可借,可還)
+        VALUES(?,?,?,?,?,?,?)
+    '''
+    cursor.execute(sql,values)
+    conn.commit()
+
+def updata_sqlite_data()->None:
+    '''
+    下載,並更新資料庫
+    '''
+    data = __download_youbike_data()
+    conn = sqlite3.connect("youbike.db")
+    __create_table(conn)
+    for item in data:
+        __insert_data(conn,[item['sna'],item['sarea'],item['mday'],item['ar'],item['tot'],item['sbi'],item['bemp']])
+    conn.close()
