2603460c7e222adaee8e81af23344e63b63bda97
cute511131 <cute511131@gmail.com>
2023-11-01 08:50:04 +0800

    commit

--- 2023.10.26/datasource.py
+++ 2023.10.26/datasource.py
@@ -27,7 +27,8 @@ def __create_table(conn:sqlite3.Connection):
             "總車輛數"	INTEGER,
             "可借"	INTEGER,
             "可還"	INTEGER,
-            PRIMARY KEY("id" AUTOINCREMENT)
+            PRIMARY KEY("id" AUTOINCREMENT),
+            UNIQUE(站點名稱,更新時間) ON CONFLICT REPLACE 
         );
         '''
     )
@@ -36,7 +37,7 @@ def __create_table(conn:sqlite3.Connection):
 def __insert_data(conn:sqlite3.Connection,values:list[any])->None:
     cursor = conn.cursor()
     sql = '''
-    INSERT INTO 台北市youbike(站點名稱,行政區,更新時間,地址,總車輛數,可借,可還)
+    REPLACE INTO 台北市youbike(站點名稱,行政區,更新時間,地址,總車輛數,可借,可還)
         VALUES(?,?,?,?,?,?,?)
     '''
     cursor.execute(sql,values)
--- 2023.10.26/index.py
+++ 2023.10.26/index.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+from threading import Timer
 import datasource
 
 class Window(tk.Tk):
@@ -12,14 +13,25 @@ class Window(tk.Tk):
             messagebox.showerror("錯誤",'網路不正常\n將關閉應用程式\n請稍後再試')
             self.destroy()
 
-        
-        
+def on_closing(w:Window):
+    print("window關閉")
+    t.cancel()
+    w.destroy()
+
+t = None
+def update_data()->None:
+    print("做事")
+    global t
+    t = Timer(20,update_data)
+    t.start()        
 
 def main():
     window = Window()
     window.title('台北市youbike2.0')
     window.geometry('600x300')
-    window.resizable(width=False,height=False)    
+    window.resizable(width=False,height=False)
+    update_data()
+    window.protocol("WM_DELETE_WINDOW",lambda :on_closing(window))       
     window.mainloop()
 
 if __name__ == '__main__':
